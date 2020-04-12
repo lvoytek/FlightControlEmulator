581ddfad3f48c66bcb65749c51b0ddfdee4d3d55
Lena Voytek <dvoytek@email.arizona.edu>
2020-04-12 16:36:11 -0700

    Changed comment style and added setduty defs

--- src/PWMHandler.h
+++ src/PWMHandler.h
@@ -1,24 +1,24 @@
 /*
-* Copyright (c) 2020 Lena Voytek
-* 
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-* 
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of the Software.
-* 
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*/
+ * Copyright (c) 2020 Lena Voytek
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 #ifndef PWMHANDLER_H
 #define PWMHANDLER_H
@@ -68,7 +68,8 @@ typedef enum
 typedef enum
 {
 	PWM_SUCCESS = 0,
-	PWM_FAILURE
+	PWM_FAILURE,
+	PWM_INVALID_CHANNEL
 } pwm_state;
 
 
@@ -93,37 +94,79 @@ protected:
 	int pwmFrequency;
 
 public:
-	/*
-	 * Set default Feather PWM pins on MCPWM unit 0
-	*/
+	/**
+	 * @brief Set default Feather PWM pins on MCPWM unit 0
+	 */
 	PWMHandler();
 
-	/*
-	 * Set PWM pins using a given MCPWM unit 
+	/**
+	 * @brief Set specified PWM pins using a given MCPWM unit 
+	 * 
+	 * @param pwmUnit The MCPWM unit to use for timing
+	 * @param channel1 PWM Channel 1 output GPIO pin
+	 * @param channel2 PWM Channel 2 output GPIO pin
+	 * @param channel3 PWM Channel 3 output GPIO pin
+	 * @param channel4 PWM Channel 4 output GPIO pin
+	 * @param channel5 PWM Channel 5 output GPIO pin
+	 * @param channel6 PWM Channel 6 output GPIO pin
 	 */
 	PWMHandler(mcpwm_unit_t pwmUnit, int channel1, int channel2, int channel3, int channel4, int channel5, int channel6);
 	
-	/*
-	 * Initialize the mcpwm unit and prepare for gpio output use
+	/**
+	 * @brief Initialize the mcpwm unit and prepare for gpio output use
 	 * 
-	 * returns PWM_SUCCESS on proper initialization and PWM_FAILURE on MCPWMn failure
+	 * @return
+	 *     - PWM_SUCCESS Initialization successful
+	 *     - PWM_FAILURE MCPWMn failure
 	 */
 	pwm_state init();
 
-	/*
-	 * Activate all PWM outputs in current configuration
+	/**
+	 * @brief Activate all PWM outputs in current configuration
 	 * 
-	 * returns PWM_SUCCESS on successful start and PWM_FAILURE otherwise
-	*/
+	 * @return
+	 *     - PWM_SUCCESS Successful start
+	 *     - PWM_FAILURE Activation failed
+	 */
 	pwm_state start();
 
-	/*
-	 * Deactivate all PWM outputs in current configuration
+	/**
+	 * @brief Deactivate all PWM outputs in current configuration
 	 * 
-	 * returns PWM_SUCCESS on successful stop and PWM_FAILURE otherwise
-	*/
+	 * @return
+	 *     - PWM_SUCCESS Successful stop
+	 *     - PWM_FAILURE Deactivation failed
+	 */
 	pwm_state stop();
 
+	/**
+	 * @brief Set the PWM duty cycles of all channels
+	 * 
+	 * @param channel1 The PWM Positive Duty % for the channel 1 pin
+	 * @param channel2 The PWM Positive Duty % for the channel 2 pin
+	 * @param channel3 The PWM Positive Duty % for the channel 3 pin
+	 * @param channel4 The PWM Positive Duty % for the channel 4 pin
+	 * @param channel5 The PWM Positive Duty % for the channel 5 pin
+	 * @param channel6 The PWM Positive Duty % for the channel 6 pin
+	 * 
+	 * @return
+	 *     - PWM_SUCCESS Successful duty change
+	 *     - PWM_FAILURE Duty change failed
+	 */
+	pwm_state setDutyAll(float channel1, float channel2, float channel3, float channel4, float channel5, float channel6);
+
+	/**
+	 * @brief Set the PWM duty cycle for the pin on the given channel
+	 * 
+	 * @param channel The channel to change the duty cycle of
+	 * @param dutyPercentage The positive duty cycle percentage to change to
+	 * 
+	 * @return
+	 *     - PWM_SUCCESS Successful duty change
+	 *     - PWM_FAILURE Duty change failed
+	 *     - PWM_INVALID_CHANNEL The given channel number is not 1-6, no change
+	 */
+	pwm_state setDuty(int channel, float dutyPercentage);
 };
 
 #endif
